b655e7a33062ef949fb0d7a86b8f98549b2118e9
Yishak <113466968+Teklemya@users.noreply.github.com>
2026-04-13 20:12:30 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (61.42%) - LeetSync

---
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        seen = {}
+        res = float(inf)
+        for i, num in enumerate(nums):
+            if num == target:
+                res = min(res, abs(i - start))    
+        return res  
+
+
+
+        '''
+        Given an int array, i will keep trakc of the value and index for target and then calcuate to take 
+        the minimum abs(i-start) and return that so first i will build a dict if num == target i will add it in dict
+        for val in dict i will claclute the min of value - start and take the min of that and return
+        '''
