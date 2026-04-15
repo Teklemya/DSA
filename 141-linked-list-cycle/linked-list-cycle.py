5e53678ec7075a43a7e3f348918f77ed4b411da2
Yishak <113466968+Teklemya@users.noreply.github.com>
2026-04-15 02:37:06 -0400

    Time: 61 ms (15.07%) | Memory: 22.5 MB (61.41%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,14 +6,23 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        #using a set to check if visted or not   
-        seen = set()
-        while head:
-            if head in seen:
+        fast, slow = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
-            seen.add(head)
-            head = head.next
         return False
+
+        #using a set to check if visted or not   
+        # seen = set()
+        # while head:
+        #     if head in seen:
+        #         return True
+        #     seen.add(head)
+        #     head = head.next
+        # return False
 
 
 
