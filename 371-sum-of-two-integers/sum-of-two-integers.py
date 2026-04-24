7dbfe173c379c4d18e1ee0ee5245424290a9494a
Yishak <113466968+Teklemya@users.noreply.github.com>
2026-04-24 18:43:44 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (55.93%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        '''
+        Given two ints i am expected to add them without using + or - which i tink the best thing to use is sum
+        i can just sum up a and b by i think imporitng math
+        '''
+
+        return sum([a,b])
