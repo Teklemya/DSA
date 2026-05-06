e88e796ea0576785a4463e40ee8c44ec0a1d0303
Yishak <113466968+Teklemya@users.noreply.github.com>
2026-05-05 23:53:51 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (17.73%) - LeetSync

---
+++ 812-rotate-string/rotate-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        seen = set()
+        currS = s 
+        print(currS)
+        while currS not in seen:
+            #add the inital s to the set to shot while loop when we see that string
+            seen.add(currS)
+            if currS == goal:
+                return True
+            else:
+                #then now i need to move the frist letter to the end
+                firstLetter = currS[0]
+                currS = currS[1:]
+                currS = currS + firstLetter
+                print(currS)
+        return False
+        '''
+        Given two strings after rotating the s string by moving the first letter to the end constatly if i hit the goal
+        then great i return true else false?
+        
+        I am thinking of haivng a case where i check 
+        i will set the currS = s initally
+        while currS not in seen:
+            if currS == goal:
+                return True
+            seen.add(currS)
+            else:
+                then now i need to move the frist letter to the end
+                fristLetter = currS[0]
+                currS = currS.join("firstLetter")
+                currS = currS.replace(firstLetter, "")
+        return False
+        '''
