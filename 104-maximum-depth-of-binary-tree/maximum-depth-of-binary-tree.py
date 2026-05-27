ce65dc47706a5e4f6ee2d91789411ca85af5a516
Yishak <113466968+Teklemya@users.noreply.github.com>
2026-05-26 21:05:09 -0400

    Time: 0 ms (100.00%) | Memory: 20.4 MB (24.53%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,23 +6,31 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        depth = 0
-        maxDepth = 0
-        #set up dfs
-        def dfs(node, depth):
-            nonlocal maxDepth # Tells Python to use the maxDepth from the outer scope
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
-            if not node:
-                return
-            # add to the depth since we see a node and the base case is not hit
-            depth += 1
-            maxDepth = max(maxDepth, depth)
-            #call dfs on children and pass in current depth as well
-            dfs(node.left, depth)
-            dfs(node.right, depth)
-        # Actually trigger the DFS traversal
-        dfs(root, depth)
 
-        return maxDepth
+
+
+
+        # depth = 0
+        # maxDepth = 0
+        # #set up dfs
+        # def dfs(node, depth):
+        #     nonlocal maxDepth # Tells Python to use the maxDepth from the outer scope
+
+        #     if not node:
+        #         return
+        #     # add to the depth since we see a node and the base case is not hit
+        #     depth += 1
+        #     maxDepth = max(maxDepth, depth)
+        #     #call dfs on children and pass in current depth as well
+        #     dfs(node.left, depth)
+        #     dfs(node.right, depth)
+        # # Actually trigger the DFS traversal
+        # dfs(root, depth)
+
+        # return maxDepth
 
         
